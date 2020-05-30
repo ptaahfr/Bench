858a59d1c837bcb003fc393e6cedf9d60c45fe6b
manu <49815417+ptaahfr@users.noreply.github.com>
2020-05-30 21:55:58 +0200

    * permutation arg is now called RANK * permutation were not taken into account (stupid me)

--- 04_Looper/Looper.cpp
+++ 04_Looper/Looper.cpp
@@ -75,21 +75,21 @@ void test_looper_unrolled()
 // 4) 2D case allowing permutation of loops, we use tuple for bounds
 
 // Class to define the 2D permutation
-template <size_t X0, size_t X1>
+template <size_t RANK0, size_t RANK1>
 class Permutation2D
 {
-    static_assert(X0 + X1 == 1 && X0 < 2 && X1 < 2, "Invalid permutation");
+    static_assert(RANK0 + RANK1 == 1 && RANK0 < 2 && RANK1 < 2, "Invalid permutation");
 };
 
-template <size_t X0, size_t X1, typename BOUND0, typename BOUND1, typename FUNC, typename... ARGS>
-void For2D(Permutation2D<X0, X1> permutation, std::tuple<BOUND0, BOUND1> const & boundsMin, std::tuple<BOUND0, BOUND1> const & boundsMax, FUNC && func, ARGS &&... args)
+template <size_t RANK0, size_t RANK1, typename BOUND0, typename BOUND1, typename FUNC, typename... ARGS>
+void For2D(Permutation2D<RANK0, RANK1> permutation, std::tuple<BOUND0, BOUND1> const & boundsMin, std::tuple<BOUND0, BOUND1> const & boundsMax, FUNC && func, ARGS &&... args)
 {
-    for (BOUND0 y = std::get<X0>(boundsMin); y < std::get<X0>(boundsMax); ++y)
+    for (BOUND0 y = std::get<RANK0>(boundsMin); y < std::get<RANK0>(boundsMax); ++y)
     {
-        for (BOUND1 x = std::get<X1>(boundsMin); x < std::get<X1>(boundsMax); ++x)
+        for (BOUND1 x = std::get<RANK1>(boundsMin); x < std::get<RANK1>(boundsMax); ++x)
         {
             auto const indices = std::make_tuple(x, y);
-            func(std::get<X0>(indices), std::get<X1>(indices), std::forward<ARGS>(args)...);
+            func(std::get<RANK0>(indices), std::get<RANK1>(indices), std::forward<ARGS>(args)...);
         }
     }
 }
@@ -141,44 +141,44 @@ auto InsertTuple(ConstSizeT<AT> at, WHAT && what, std::tuple<ELEMENTS...> const 
 // Utilities used only to make sure the permutation validity is checked at compile time
 namespace details
 {
-    template <size_t X0, size_t... XN>
-    class ExpectedPermutationSum : public std::integral_constant<size_t, ExpectedPermutationSum<XN...>::value + sizeof...(XN)>
+    template <size_t RANK0, size_t... RANKS>
+    class ExpectedPermutationSum : public std::integral_constant<size_t, ExpectedPermutationSum<RANKS...>::value + sizeof...(RANKS)>
     {
     };
 
-    template <size_t XN>
-    class ExpectedPermutationSum<XN> : public std::integral_constant<size_t, 0>
+    template <size_t RANKS>
+    class ExpectedPermutationSum<RANKS> : public std::integral_constant<size_t, 0>
     {
 
     };
 
-    template <size_t X0, size_t... XN>
-    class SumOfElements : public std::integral_constant<size_t, SumOfElements<XN...>::value + X0>
+    template <size_t RANK0, size_t... RANKS>
+    class SumOfElements : public std::integral_constant<size_t, SumOfElements<RANKS...>::value + RANK0>
     {
     };
 
-    template <size_t X0>
-    class SumOfElements<X0> : public std::integral_constant<size_t, X0>
+    template <size_t RANK0>
+    class SumOfElements<RANK0> : public std::integral_constant<size_t, RANK0>
     {
     };
 
-    template <size_t MAX_VALUE, size_t X0, size_t... XN>
-    class AreValuesUnder : public std::integral_constant<bool, (X0 < MAX_VALUE) && AreValuesUnder<MAX_VALUE, XN...>::value>
+    template <size_t MAX_VALUE, size_t RANK0, size_t... RANKS>
+    class AreValuesUnder : public std::integral_constant<bool, (RANK0 < MAX_VALUE) && AreValuesUnder<MAX_VALUE, RANKS...>::value>
     {
     };
 
-    template <size_t MAX_VALUE, size_t X0>
-    class AreValuesUnder<MAX_VALUE, X0> : public std::integral_constant<bool, (X0 < MAX_VALUE)>
+    template <size_t MAX_VALUE, size_t RANK0>
+    class AreValuesUnder<MAX_VALUE, RANK0> : public std::integral_constant<bool, (RANK0 < MAX_VALUE)>
     {
     };
 }
 
 // 5) ND case allowing permutation for loops
-template <size_t... XN>
+template <size_t... RANKS>
 class Permutation
 {
-    static_assert(details::SumOfElements<XN...>::value == details::ExpectedPermutationSum<XN...>::value
-               && details::AreValuesUnder<sizeof...(XN), XN...>::value, "Invalid permutation");
+    static_assert(details::SumOfElements<RANKS...>::value == details::ExpectedPermutationSum<RANKS...>::value
+               && details::AreValuesUnder<sizeof...(RANKS), RANKS...>::value, "Invalid permutation");
 };
 
 
@@ -192,22 +192,26 @@ namespace details
     }
 
     // End of the recursion (LOOP_LEVEL == MAX_LOOP_LEVEL)
-    template <size_t MAX_LOOP_LEVEL, size_t... XN, typename... BOUNDS, typename FUNC, typename... ARGS>
-    static void ForNSimple(ConstSizeT<MAX_LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<BOUNDS...> const & indices, Permutation<XN...> permutation,
+    template <size_t MAX_LOOP_LEVEL, size_t... RANKS, typename... BOUNDS, typename FUNC, typename... ARGS>
+    static void ForNSimple(ConstSizeT<MAX_LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<BOUNDS...> const & indices, Permutation<RANKS...> permutation,
         std::tuple<BOUNDS...> const & boundsMin, std::tuple<BOUNDS...> const & boundsMax, FUNC && func, ARGS &&... args)
     {
-        CallFunc(std::make_index_sequence<sizeof...(XN)>(), indices, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
+        CallFunc(std::make_index_sequence<sizeof...(RANKS)>(), indices, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
     }
 
     // Recursive definition, we build at each step the indices tuple and increase the loopLevel
     // At each level we enumerate index and insert it at the right position in the "indices" tuple.
-    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, size_t... XN, typename... BOUNDS, typename FUNC, typename... ARGS>
-    static void ForNSimple(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<BOUNDS...> const & indices, Permutation<XN...> permutation,
+    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, size_t... RANKS, typename... BOUNDS, typename FUNC, typename... ARGS>
+    static void ForNSimple(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<BOUNDS...> const & indices, Permutation<RANKS...> permutation,
         std::tuple<BOUNDS...> const & boundsMin, std::tuple<BOUNDS...> const & boundsMax, FUNC && func, ARGS &&... args)
     {
-        for (auto index = std::get<LOOP_LEVEL>(boundsMin); index < std::get<LOOP_LEVEL>(boundsMax); ++index)
+        // Get the current rank to unloop
+        auto const rank{ std::get<LOOP_LEVEL>(std::make_tuple(ConstSizeT<RANKS>()...)) }; // ~ ConstSizeT<RANK> rank
+        enum { RANK = decltype(rank)::value };
+
+        for (auto index = std::get<RANK>(boundsMin); index < std::get<RANK>(boundsMax); ++index)
         {
-            ForNSimple(ConstSizeT<LOOP_LEVEL + 1>(), maxLoopLevel, InsertTuple(loopLevel, index, indices), permutation,
+            ForNSimple(ConstSizeT<LOOP_LEVEL + 1>(), maxLoopLevel, InsertTuple(rank, index, indices), permutation,
                 boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
         }
     }
@@ -215,10 +219,10 @@ namespace details
 
 // Entry point, simply calls the recursive function adding arguments needed for recursive state
 // We initialize indices with boundsMin
-template <size_t... XN, typename... BOUNDS, typename FUNC, typename... ARGS>
-void ForNSimple(Permutation<XN...> permutation, std::tuple<BOUNDS...> const & boundsMin, std::tuple<BOUNDS...> const & boundsMax, FUNC && func, ARGS &&... args)
+template <size_t... RANKS, typename... BOUNDS, typename FUNC, typename... ARGS>
+void ForNSimple(Permutation<RANKS...> permutation, std::tuple<BOUNDS...> const & boundsMin, std::tuple<BOUNDS...> const & boundsMax, FUNC && func, ARGS &&... args)
 {
-    details::ForNSimple(ConstSizeT<0>(), ConstSizeT<sizeof...(XN)>(), boundsMin, permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
+    details::ForNSimple(ConstSizeT<0>(), ConstSizeT<sizeof...(RANKS)>(), boundsMin, permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
 }
 
 void test_looper_for_permuted_nd()
@@ -234,47 +238,53 @@ void test_looper_for_permuted_nd()
 namespace details
 {
     // End of the recursion (LOOP_LEVEL == MAX_LOOP_LEVEL)
-    template <size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... XN, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
-    static void ForN(ConstSizeT<MAX_LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<XN...> permutation,
+    template <size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... RANKS, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
+    static void ForN(ConstSizeT<MAX_LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<RANKS...> permutation,
         std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args)
     {
-        CallFunc(std::make_index_sequence<sizeof...(XN)>(), indices, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
+        CallFunc(std::make_index_sequence<sizeof...(RANKS)>(), indices, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
     }
 
     // We need to declare this before the following lambda.
-    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... XN, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
-    static void ForN(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<XN...> permutation,
+    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... RANKS, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
+    static void ForN(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<RANKS...> permutation,
         std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args);
 
     // "Lambda" (as a function because we need variadic args...) to call the for recursion
     class ForNToFor1D
     {
     public:
-        template <typename INDEX, size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... XN, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
-        void operator()(INDEX index, ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<XN...> permutation,
+        template <typename INDEX, size_t RANK, size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES,
+            size_t... RANKS, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
+        void operator()(INDEX index, ConstSizeT<RANK> rank, ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel,
+            std::tuple<INDICES...> const & indices, Permutation<RANKS...> permutation,
             std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args) const
         {
-            ForN(ConstSizeT<LOOP_LEVEL + 1>(), maxLoopLevel, InsertTuple(loopLevel, index, indices),
+            ForN(ConstSizeT<LOOP_LEVEL + 1>(), maxLoopLevel, InsertTuple(rank, index, indices),
                 permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
         }
     };
 
     // Recursive definition, we build at each step the indices tuple and increase the loopLevel
     // This one using For1D so we can benefit For1D specialization, we need to pass by a functor (above), because this is C++14 and variadic are not supported in lambdas
-    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... XN, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
-    static void ForN(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<XN...> permutation,
+    template <size_t LOOP_LEVEL, size_t MAX_LOOP_LEVEL, typename... INDICES, size_t... RANKS, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
+    static void ForN(ConstSizeT<LOOP_LEVEL> loopLevel, ConstSizeT<MAX_LOOP_LEVEL> maxLoopLevel, std::tuple<INDICES...> const & indices, Permutation<RANKS...> permutation,
         std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args)
     {
-        For1D(std::get<LOOP_LEVEL>(boundsMin), std::get<LOOP_LEVEL>(boundsMax), ForNToFor1D(),
+        // Get the current rank to unloop
+        auto const rank{ std::get<LOOP_LEVEL>(std::make_tuple(ConstSizeT<RANKS>()...)) }; // ~ ConstSizeT<RANK> rank
+        enum { RANK = decltype(rank)::value };
+
+        For1D(std::get<RANK>(boundsMin), std::get<RANK>(boundsMax), ForNToFor1D(), rank,
             loopLevel, maxLoopLevel, indices, permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
     }
 }
 
 // Entry point, simply calls the recursive function adding arguments needed for recursive state
-template <size_t... XN, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
-void ForN(Permutation<XN...> permutation, std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args)
+template <size_t... RANKS, typename... BOUNDS_MIN, typename... BOUNDS_MAX, typename FUNC, typename... ARGS>
+void ForN(Permutation<RANKS...> permutation, std::tuple<BOUNDS_MIN...> const & boundsMin, std::tuple<BOUNDS_MAX...> const & boundsMax, FUNC && func, ARGS &&... args)
 {
-    details::ForN(ConstSizeT<0>(), ConstSizeT<sizeof...(XN)>(), boundsMin, permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
+    details::ForN(ConstSizeT<0>(), ConstSizeT<sizeof...(RANKS)>(), boundsMin, permutation, boundsMin, boundsMax, std::forward<FUNC>(func), std::forward<ARGS>(args)...);
 }
 
 void test_looper_for_permuted_nd_withunroll()
